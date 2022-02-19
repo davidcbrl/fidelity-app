8de9a35356fc5d87159621d7839064b5a2347623
rhonner <rhonner.matheus@gmail.com>
2022-02-19 14:11:05 -0300

    settings_page.dart added

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -10,19 +10,84 @@ class SettingsPage extends StatelessWidget {
         title: 'Ajustes',
         hasBackButton: false,
       ),
-      body: SettingsBody(),
+      body: Container(child: SettingsBody()),
     );
   }
 }
 
-class SettingsBody extends StatefulWidget {
-  @override
-  _SettingsBodyState createState() => _SettingsBodyState();
-}
-
-class _SettingsBodyState extends State<SettingsBody> {
+class SettingsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          SettingItem("Perfil", "Visualize os dados sobre o seu cadastro"),
+          SizedBox(
+            height: 10,
+          ),
+          SettingItem("Funcionarios", "Gerencie perfis de funcionarios"),
+          SizedBox(
+            height: 10,
+          ),
+          SettingItem("Tema", "Customize a aparencia do app"),
+          SizedBox(
+            height: 10,
+          ),
+          SettingItem("Sobre", "Visualize informacoes sobre o app"),
+          SizedBox(
+            height: 10,
+          ),
+          SettingItem("Sair", "Encerrar sua sessao"),
+        ],
+      ),
+    );
+  }
+
+  Widget SettingItem(String name, String desc) {
+    return Container(
+      child: Container(
+        height: 70,
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: Row(
+          children: [
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text(name!),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      // Text(
+                      //   "Acesso restrito*",
+                      //   style: TextStyle(color: Colors.red),
+                      // )
+                    ],
+                  ),
+                  Text(desc!),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.chevron_right_outlined,
+              color: Colors.grey.shade400,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
