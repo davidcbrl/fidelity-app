84935bc7e4025421713b7c9c421d3dd8972b0091
David Cabral <david.pereira@tecnofit.com.br>
2022-02-20 01:12:30 -0300

    Dashboard empty state

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -1,6 +1,8 @@
+import 'package:fidelity/controllers/auth_controller.dart';
 import 'package:fidelity/widgets/fidelity_appbar.dart';
 import 'package:fidelity/widgets/fidelity_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DashboardPage extends StatelessWidget {
   @override
@@ -21,8 +23,85 @@ class DashboardBody extends StatefulWidget {
 }
 
 class _DashboardBodyState extends State<DashboardBody> {
+  AuthController authController = Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.secondaryVariant,
+                ),
+              ),
+              child: Image.asset(
+                'assets/img/company.png',
+                width: 50,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  authController.user.name ?? 'Luke Skywalker LTDA',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                Text(
+                  'Bem vindo!',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        Stack(
+          children: [
+            Image.asset(
+              'assets/img/dashboard.gif',
+            ),
+            Positioned(
+              right: 0,
+              child: Container(
+                width: 125,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Esta é sua dashboard',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Aqui serão exibidos dados estatísticos de produtos e fidelidades ativas para auxiliar o seu negócio',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
