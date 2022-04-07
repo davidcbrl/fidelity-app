fdf42cac6080a18610c0b53d52eab8d287edb465
David Cabral <david.pereira@tecnofit.com.br>
2022-04-06 22:23:54 -0300

    Fixing password reset

--- lib/pages/auth/password_reset_page.dart
+++ lib/pages/auth/password_reset_page.dart
@@ -1,6 +1,7 @@
 import 'package:fidelity/controllers/auth_controller.dart';
 import 'package:fidelity/widgets/fidelity_appbar.dart';
 import 'package:fidelity/widgets/fidelity_button.dart';
+import 'package:fidelity/widgets/fidelity_loading.dart';
 import 'package:fidelity/widgets/fidelity_page.dart';
 import 'package:fidelity/widgets/fidelity_text_button.dart';
 import 'package:fidelity/widgets/fidelity_text_field_masked.dart';
@@ -26,57 +27,64 @@ class PasswordResetBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Insira o e-mail vinculado à sua conta, enviaremos uma nova senha',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                FidelityTextFieldMasked(
-                  controller: _emailController,
-                  label: 'E-mail',
-                  placeholder: 'skywalker@jedi.com',
-                  icon: Icon(Icons.email_outlined),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Campo vazio';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    if (value.isNotEmpty) _formKey.currentState!.validate();
-                  },
-                ),
-              ],
+    return Obx(
+      () => authController.loading.value
+      ? FidelityLoading(
+          loading: authController.loading.value,
+          text: 'Redefinindo senha...',
+        )
+      : Column(
+        children: [
+          Expanded(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Insira o e-mail vinculado à sua conta, enviaremos uma nova senha',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FidelityTextFieldMasked(
+                    controller: _emailController,
+                    label: 'E-mail',
+                    placeholder: 'skywalker@jedi.com',
+                    icon: Icon(Icons.email_outlined),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Campo vazio';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      if (value.isNotEmpty) _formKey.currentState!.validate();
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        FidelityButton(
-          label: 'Receber senha',
-          onPressed: () {
-            reset(context);
-          },
-        ),
-        FidelityTextButton(
-          label: 'Voltar',
-          onPressed: () {
-            Get.back();
-          },
-        ),
-      ],
+          SizedBox(
+            height: 20,
+          ),
+          FidelityButton(
+            label: 'Receber senha',
+            onPressed: () {
+              reset(context);
+            },
+          ),
+          FidelityTextButton(
+            label: 'Voltar',
+            onPressed: () {
+              Get.back();
+            },
+          ),
+        ],
+      ),
     );
   }
 
@@ -88,7 +96,7 @@ class PasswordResetBody extends StatelessWidget {
       await authController.reset();
       if (authController.status.isSuccess) {
         authController.loading.value = false;
-        Get.toNamed('/reset_success');
+        Get.toNamed('/auth/reset_success');
         return;
       }
       authController.loading.value = false;
