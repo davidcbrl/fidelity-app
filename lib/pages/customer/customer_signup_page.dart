0f7a22e33e507e1360207f84e2f98cc952ea2024
rhonner <rhonner.matheus@gmail.com>
2022-08-03 21:56:31 -0300

    changes  - email validator and confirm password validator to client

--- lib/pages/customer/customer_signup_page.dart
+++ lib/pages/customer/customer_signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:fidelity/controllers/customer_controller.dart';
 import 'package:fidelity/models/customer.dart';
 import 'package:fidelity/models/user.dart';
@@ -35,49 +36,47 @@ class CustomerSignupBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Obx(
       () => customerController.loading.value
-      ? FidelityLoading(
-          loading: customerController.loading.value,
-          text: 'Cadastrando-se...',
-        )
-      : Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                'Insira seus dados para criar uma conta',
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Form(
-                  key: _formCustomerSignupKey,
-                  child: _formFields(),
+          ? FidelityLoading(
+              loading: customerController.loading.value,
+              text: 'Cadastrando-se...',
+            )
+          : Column(
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-              ),
+                Center(
+                  child: Text(
+                    'Insira seus dados para criar uma conta',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Form(
+                      key: _formCustomerSignupKey,
+                      child: _formFields(),
+                    ),
+                  ),
+                ),
+                FidelityButton(
+                    label: 'Salvar',
+                    onPressed: () async {
+                      saveCustomer(context);
+                    }),
+                FidelityTextButton(
+                    label: 'Voltar',
+                    onPressed: () {
+                      Get.back();
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
-            FidelityButton(
-              label: 'Salvar',
-              onPressed: () async {
-                saveCustomer(context);
-              }
-            ),
-            FidelityTextButton(
-              label: 'Voltar',
-              onPressed: () {
-                Get.back();
-              }
-            ),
-            SizedBox(
-              height: 10,
-            ),
-          ],
-        ),
     );
   }
 
@@ -100,7 +99,7 @@ class CustomerSignupBody extends StatelessWidget {
             return null;
           },
           onChanged: (value) {
-            if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
+            //if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
           },
         ),
         SizedBox(
@@ -118,7 +117,7 @@ class CustomerSignupBody extends StatelessWidget {
             return null;
           },
           onChanged: (value) {
-            if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
+            //  if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
           },
         ),
         SizedBox(
@@ -133,10 +132,11 @@ class CustomerSignupBody extends StatelessWidget {
             if (value == null || value.isEmpty) {
               return 'Campo vazio';
             }
+            if (!EmailValidator.validate(value)) return 'email inválido';
             return null;
           },
           onChanged: (value) {
-            if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
+            // if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
           },
         ),
         SizedBox(
@@ -155,7 +155,7 @@ class CustomerSignupBody extends StatelessWidget {
             return null;
           },
           onChanged: (value) {
-            if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
+            // if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
           },
         ),
         SizedBox(
@@ -171,10 +171,11 @@ class CustomerSignupBody extends StatelessWidget {
             if (value == null || value.isEmpty) {
               return 'Campo vazio';
             }
+            if (value != _passwordController.text) return 'confirmação diferente da senha';
             return null;
           },
           onChanged: (value) {
-            if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
+            // if (value.isNotEmpty) _formCustomerSignupKey.currentState!.validate();
           },
         ),
         SizedBox(
@@ -192,7 +193,7 @@ class CustomerSignupBody extends StatelessWidget {
         email: _emailController.text,
         password: _passwordController.text,
         customer: new Customer(
-        name: _nameController.text,
+          name: _nameController.text,
           cpf: _cpfController.text,
         ),
       );
@@ -218,12 +219,11 @@ class CustomerSignupBody extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: FidelityButton(
-                label: 'OK',
-                width: double.maxFinite,
-                onPressed: () {
-                  Get.back();
-                }
-              ),
+                  label: 'OK',
+                  width: double.maxFinite,
+                  onPressed: () {
+                    Get.back();
+                  }),
             ),
           ],
         ),
